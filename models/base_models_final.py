af63c3864248bd767705f8541753a666bbb73c38
Jolu <joselgs96@gmail.com>
2026-06-30 11:51:53 +0200

    base_models_final.py -> Old and deprecated models removed and CustomResNetBinary and AttentionMILHead adapted to no contribution map with no bias

--- models/base_models_final.py
+++ models/base_models_final.py
@@ -7,6 +7,11 @@ from typing import List, Tuple, Dict, Any, Optional, Literal
 ################### UTILITY FUNCTIONS FOR MODEL MODIFICATION ###################
 # ALLOWS TO GET THE MODULES OF A MODEL BY NAME  
 def set_module_by_name(model: nn.Module, name: str, new_module: nn.Module):
+    """
+    Replaces a submodule within a PyTorch model using its dot-separated name.
+
+    """
+    
     parts = name.split('.')
     parent = model
     for p in parts[:-1]:
@@ -15,6 +20,21 @@ def set_module_by_name(model: nn.Module, name: str, new_module: nn.Module):
 
 
 def adapt_first_conv_to_n_channels(model: nn.Module, in_channels: int, copy_weights = True) -> nn.Module:
+    """
+    Modifies the first Conv2d layer of a model to accept a new number of input channels.
+
+    Args:
+        model (nn.Module): The model to modify.
+        in_channels (int): The new number of input channels.
+        copy_weights (bool): Whether to copy and adapt existing weights. Defaults to True.
+
+    Returns:
+        nn.Module: The modified model.
+
+    Raises:
+        RuntimeError: If no Conv2d layer is found in the model.
+    """
+    
     first_name, first_conv = None, None
     for name, m in model.named_modules():
         if isinstance(m, nn.Conv2d):
@@ -62,80 +82,7 @@ def adapt_first_conv_to_n_channels(model: nn.Module, in_channels: int, copy_weig
     set_module_by_name(model, first_name, new_conv)
     return model
 
-class VGG16(nn.Module):
-    def __init__(self, num_classes, xtype_features=4, in_channels=3, pretrained=True):
-        super().__init__()
-        vgg_base = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1 if pretrained else None)
-        self.features = vgg_base.features
-        self.features = adapt_first_conv_to_n_channels(self.features, in_channels)
-        self.adaptive_pool = nn.AdaptiveAvgPool2d((7, 7))
-        self.base_classifier = nn.Sequential(*list(vgg_base.classifier.children())[:-1])
-        self.lastFC = nn.Linear(4096 + xtype_features, num_classes)
 
-    def forward(self, x, xtype):
-        x = self.features(x)
-        x = self.adaptive_pool(x)
-        x = x.view(x.size(0), -1)
-        image_features = self.base_classifier(x)
-        xtype = xtype.view(xtype.size(0), -1)
-        return self.lastFC(torch.cat((image_features, xtype), dim=1))
-
-
-
-
-
-
-
-
-
-
-
-
-class LSEPoolingHead(nn.Module):
-    """
-    KxK conv -> per-location logit map -> LogSumExp(mean) over H*W (size-invariant).
-    """
-    def __init__(self, in_ch: int, k: int = 3, bias: bool = True, stabilize: bool = True):
-        super().__init__()
-        self.head = nn.Conv2d(in_ch, 1, kernel_size=k, padding=k//2, bias=bias)
-        self.stabilize = stabilize
-
-    def forward(self, feat: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        logit_map = self.head(feat)            # [B,1,H',W']
-        L = logit_map.flatten(2)               # [B,1,N]
-        if self.stabilize:
-            m = L.max(dim=2, keepdim=True).values
-            lse = m + torch.log(torch.exp(L - m).mean(dim=2, keepdim=True))
-        else:
-            lse = torch.log(torch.exp(L).mean(dim=2, keepdim=True))
-        return lse.squeeze(dim=2), logit_map                  # [B,1], [B,1,H',W']
-
-
-# class AttentionMILHead(nn.Module):
-#     """
-#     1x1 projection -> attention weights over HxW -> weighted sum -> linear logit.
-#     """
-#     def __init__(self, in_ch: int, proj_dim: int = 512, attn_hidden: int = 256):
-#         super().__init__()
-#         self.proj = nn.Conv2d(in_ch, proj_dim, kernel_size=1)
-#         self.attn = nn.Sequential(
-#             nn.Conv2d(proj_dim, attn_hidden, 1),
-#             nn.Tanh(),
-#             nn.Conv2d(attn_hidden, 1, 1),
-#         )
-#         self.cls = nn.Linear(proj_dim, 1)
-
-#     # def forward(self, feat: torch.Tensor, xtype: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-#     def forward(self, feat: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:        
-#         A = self.proj(feat)                    # [B,D,H',W']
-#         w = self.attn(A)                       # [B,1,H',W']
-#         alpha = F.softmax(w.flatten(2), dim=2) # [B,1,N]
-#         Aflat = A.flatten(2).transpose(1, 2)   # [B,N,D]
-#         z = torch.bmm(alpha, Aflat).squeeze(1)  # [B,D]
-#         # logit = self.cls(torch.cat((z, xtype), dim=1)).unsqueeze(1)       # [B,1]
-#         logit = self.cls(z).unsqueeze(1)       # [B,1]        
-#         attn_map = alpha.view_as(w)            # [B,1,H',W']
-#         return logit.squeeze(dim=2), attn_map
 
 
 class AttentionMILHead(nn.Module):
@@ -202,33 +149,38 @@ class AttentionMILHead(nn.Module):
         return logit, attn_map, importance_map, alt_map
 
 
-class CustomResNetBinary34(nn.Module):
-    def __init__(self, num_classes, in_channels=3):
-        super().__init__()
-        self.base_model = models.resnet34(weights=models.ResNet34_Weights.DEFAULT)
-        self.base_model = adapt_first_conv_to_n_channels(self.base_model, in_channels)
-        num_ftrs = self.base_model.fc.in_features
-        self.base_model.fc = nn.Linear(num_ftrs, 512)
-        self.lastFC = nn.Linear(512+4, num_classes)
-
-    def forward(self, x, xtype):
-        x = self.base_model(x)
-        return self.lastFC(torch.cat((x, xtype), 1))
-
-
-
 
 ##### ADAPTED MODELS ######
+# class CustomResNetBinary(nn.Module):
+#     def __init__(self, num_classes, in_channels=3 ,head: Literal['attn']='attn', k: int = 5,
+#                 proj_dim: int = 512, attn_hidden: int = 256):
+#         super().__init__()
+#         net = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+#         self.base_model = nn.Sequential(*list(net.children())[:-2])
+#         # self.base_model = adapt_first_conv_to_n_channels(self.base_model, in_channels)
+#         if head == 'attn':
+#             self.head = AttentionMILHead(512, proj_dim=proj_dim, attn_hidden=attn_hidden)
+#         else:
+#             raise ValueError("head must be 'lse' or 'attn'")
+#         self.head_name = head
+#     def forward(self, x, xtype):
+#         """
+#         x: [B, 3, H, W] 
+#         """
+#         feat = self.base_model(x)
+#         logit, map_att, map_imp, map_no_bias = self.head(feat)
+#         return logit, map_att, map_imp, map_no_bias
+    
+
+
 class CustomResNetBinary(nn.Module):
-    def __init__(self, num_classes, in_channels=3 ,head: Literal['lse','attn']='attn', k: int = 5,
+    def __init__(self, num_classes, in_channels=3 ,head: Literal['attn']='attn',
                 proj_dim: int = 512, attn_hidden: int = 256):
         super().__init__()
         net = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
         self.base_model = nn.Sequential(*list(net.children())[:-2])
         # self.base_model = adapt_first_conv_to_n_channels(self.base_model, in_channels)
-        if head == 'lse':
-            self.head = LSEPoolingHead(512, k=k)
-        elif head == 'attn':
+        if head == 'attn':
             self.head = AttentionMILHead(512, proj_dim=proj_dim, attn_hidden=attn_hidden)
         else:
             raise ValueError("head must be 'lse' or 'attn'")
@@ -238,8 +190,9 @@ class CustomResNetBinary(nn.Module):
         x: [B, 3, H, W] 
         """
         feat = self.base_model(x)
-        logit, map_att, map_imp, map_no_bias = self.head(feat)
-        return logit, map_att, map_imp, map_no_bias
+        logit, map_att, map_contrib, _ = self.head(feat)
+        return logit, map_att, map_contrib
+
 
 class CustomResNetBinary50(nn.Module):
     def __init__(self, num_classes, in_channels=3, head: Literal['lse','attn']='attn', k: int = 5,
@@ -248,12 +201,10 @@ class CustomResNetBinary50(nn.Module):
         net = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
         self.base_model = nn.Sequential(*list(net.children())[:-2])  # drop avgpool & fc
 
-        if head == 'lse':
-            self.head = LSEPoolingHead(2048, k=k)
-        elif head == 'attn':
+        if head == 'attn':
             self.head = AttentionMILHead(2048, proj_dim=proj_dim, attn_hidden=attn_hidden, Bias=Bias)
         else:
-            raise ValueError("head must be 'lse' or 'attn'")
+            raise ValueError("head must be 'attn'")
         self.head_name = head
 
     def forward(self, x, xtype):
@@ -264,7 +215,7 @@ class CustomResNetBinary50(nn.Module):
         logit, map_att, map_imp, map_no_bias = self.head(feat)          # [B,1], [B,1,H',W']
         # prob = torch.sigmoid(logit)
         # key = 'logit_map' if self.head_name=='lse' else 'attn_map'
-        return logit, map_att, map_imp, map_no_bias
+        return logit, map_att, map_imp, map_no_bias #Cambiar map_imp por map_contrib y quitar el map_no_bias
 
 
 
@@ -277,12 +228,10 @@ class CustomDenseNet(nn.Module):
         self.base_model = net.features
         feature_dim = net.classifier.in_features
 
-        if head == 'lse':
-            self.head = LSEPoolingHead(feature_dim, k=k)
-        elif head == 'attn':
+        if head == 'attn':
             self.head = AttentionMILHead(feature_dim, proj_dim=proj_dim, attn_hidden=attn_hidden)
         else:
-            raise ValueError("head must be 'lse' or 'attn'")
+            raise ValueError("head must be 'attn'")
         self.head_name = head
 
     def forward(self, x, xtype):
@@ -301,12 +250,10 @@ class CustomMobileNetV3(nn.Module):
         self.base_model = net.features
         feature_dim = net.classifier[0].in_features
         
-        if head == 'lse':
-            self.head = LSEPoolingHead(feature_dim, k=k)
-        elif head == 'attn':
+        if head == 'attn':
             self.head = AttentionMILHead(feature_dim, proj_dim=proj_dim, attn_hidden=attn_hidden)
         else:
-            raise ValueError("head must be 'lse' or 'attn'")
+            raise ValueError("head must be 'attn'")
         self.head_name = head
 
 
@@ -327,12 +274,10 @@ class EfficientNetB0(nn.Module):
 
         features_dim = net.classifier[1].in_features
 
-        if head == 'lse':
-            self.head = LSEPoolingHead(features_dim, k=k)
-        elif head == 'attn':
+        if head == 'attn':
             self.head = AttentionMILHead(features_dim, proj_dim=proj_dim, attn_hidden=attn_hidden)
         else:
-            raise ValueError("head must be 'lse' or 'attn'")
+            raise ValueError("head must be 'attn'")
         self.head_name = head
 
     def forward(self, x, xtype):
